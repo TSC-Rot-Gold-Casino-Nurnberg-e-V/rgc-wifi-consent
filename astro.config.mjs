e4eb5a9bb529291aad4980c9706609a093389546
Jonas Schlak <jschlak@freenet.de>
2023-09-17 19:12:43 +0200

    flatterns the output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  build:{
+    format: "file",
+    assets: 'captiveportal-/../',
+    inlineStylesheets: `always`,
+  },
   integrations: [tailwind(), react()]
 });
